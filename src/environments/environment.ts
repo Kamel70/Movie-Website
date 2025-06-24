abc3157d7d4f9adb1b93180221961da1c7a8326b
kamel <kamelsamy3@gmail.com>
2025-06-24 23:53:03 +0300

    edit tv show details page make it more responsive

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  apiKey:
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWZlNjNkMjljNmU3YmE4YTNlZTRlMjNiNzg4NzU4ZSIsIm5iZiI6MTc0NzY1NTc0Ny4wMTQwMDAyLCJzdWIiOiI2ODJiMWM0MzYzZmMzYzIyOGEyZjczYjIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.C066nZMrWFwGYXKX-yFX3BdgjH6vR9tvL3N_mjZIDPA',
+  apiKey: '',
   tmdbBaseUrl: 'https://api.themoviedb.org/3/',
 };
